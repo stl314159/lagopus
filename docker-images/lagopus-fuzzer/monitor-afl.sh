c0bc8eeb80801b598ee1174091bdccb33829e0a2
stl314159 <stl314159@users.noreply.github.com>
2022-05-20 23:17:05 -0400

    Add temporary JOB_ID variable

--- docker-images/lagopus-fuzzer/monitor-afl.sh
+++ docker-images/lagopus-fuzzer/monitor-afl.sh
@@ -118,7 +118,8 @@ echo "Pushing to database $INFLUX_DATABASE"
 
 # Push to InfluxDB
 TAGS="job_id=$JOB_ID"
-TAGS="$TAGS,target=$(basename "$(echo "$command_line" | sed -n 's/^.*-- //p')" | sed 's/\([ ,=]\)/\\\1/g')"
+# TAGS="$TAGS,target=$(basename "$(echo "$command_line" | sed -n 's/^.*-- //p')" | sed 's/\([ ,=]\)/\\\1/g')"
+TAGS="$TAGS,target=$JOB_ID"
 TAGS="$TAGS,host=$(hostname)"
 
 FIELDS="alive=$ALIVE_CNT"
